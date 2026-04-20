d40b88805df8f348efc6a712ff21e30a4a918f7c
jafwellbot-crypto <jafwellbot@gmail.com>
2026-04-20 14:23:16 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,55 +1,30 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 from symptom_engine import SymptomEngine
 from medicine_filter import MedicineFilter
 from doctor_locator import DoctorLocator
 
-def main():
+app = Flask(__name__)
+CORS(app)
 
-    symptom_engine = SymptomEngine()
-    medicine_filter = MedicineFilter()
-    doctor_locator = DoctorLocator()
+# Initialize modules
+symptom_engine = SymptomEngine()
+medicine_filter = MedicineFilter()
+doctor_locator = DoctorLocator()
 
-    print("AI Healthcare Assistant")
-    print("------------------------")
 
-    symptom = input("Enter your symptom: ")
+# Home route
+@app.route("/")
+def home():
+    return "AI Healthcare Assistant API Running"
 
-    results = symptom_engine.analyze_symptom(symptom)
 
-    if not results:
-        print("Sorry, we are currently not working on this symptom yet.")
-        return
+# Analyze symptom route
+@app.route("/analyze", methods=["POST"])
+def analyze():
 
-    for result in results:
+    data = request.get_json()
 
-        print("\nPossible Condition:", result["condition"])
-        print("Severity Level:", result["severity"])
-
-        if result["severity"] == "low" or result["severity"] == "medium":
-
-            medicines = medicine_filter.recommend_medicine(symptom)
-
-            if medicines:
-                print("\nSuggested Medicines:")
-
-                for med in medicines:
-                    print("-", med["medicine"], "(Avoid for:", med["avoid_for"], ")")
-
-        if result["severity"] == "high":
-
-            print("\nThis may require doctor consultation.")
-            print("\nDoctors you can consult:")
-
-            doctors = doctor_locator.get_doctors()
-
-            for doc in doctors:
-                print(
-                    doc["name"],
-                    "-",
-                    doc["specialization"],
-                    "(",
-                    doc["hospital"],
-                    ")"
-                )
-
-if __name__ == "__main__":
-    main()
+    if not data or "symptom" not in data:
+        return jsonify({"error": "No
