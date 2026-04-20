dc7a0c4c92469ed00375c117c8655f6fcc97465a
jafwellbot-crypto <jafwellbot@gmail.com>
2026-04-20 13:56:38 +0530

    Update medicine_filter.py

--- backend/medicine_filter.py
+++ backend/medicine_filter.py
@@ -1,1 +1,22 @@
+import csv
 
+class MedicineFilter:
+
+    def __init__(self):
+        self.medicine_data = []
+        self.load_data()
+
+    def load_data(self):
+        with open("data/medicines.csv", "r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                self.medicine_data.append(row)
+
+    def recommend_medicine(self, symptom):
+        medicines = []
+
+        for entry in self.medicine_data:
+            if symptom.lower() in entry["used_for"].lower():
+                medicines.append(entry)
+
+        return medicines
