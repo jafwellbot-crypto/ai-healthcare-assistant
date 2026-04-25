c9bf50636a611ecb7c61a7acf675aafc148c0b38
jafwellbot-crypto <jafwellbot@gmail.com>
2026-04-25 14:45:45 +0530

    Update symptom_engine.py

--- backend/symptom_engine.py
+++ backend/symptom_engine.py
@@ -1,4 +1,6 @@
 import csv
+import os
+
 
 class SymptomEngine:
 
@@ -7,16 +9,27 @@ class SymptomEngine:
         self.load_data()
 
     def load_data(self):
-        with open("data/symptoms.csv", "r") as file:
+
+        # Get project root directory
+        BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+        # Path to CSV file
+        data_path = os.path.join(BASE_DIR, "data", "symptoms.csv")
+
+        # Load CSV
+        with open(data_path, "r") as file:
             reader = csv.DictReader(file)
             for row in reader:
                 self.symptom_data.append(row)
 
     def analyze_symptom(self, symptom):
+
         results = []
 
         for entry in self.symptom_data:
+
             if symptom.lower() == entry["symptom"].lower():
+
                 results.append({
                     "condition": entry["possible_condition"],
                     "severity": entry["severity"]
